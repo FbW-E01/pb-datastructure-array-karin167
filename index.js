662db75c34165f53aa1dc15fe6c8ffa5457381c1
karin yahud <karin167bz@gmail.com>
2021-04-13 12:11:14 +0200

    karin-task

---
+++ index.js
@@ -0,0 +1,11 @@
+//1
+const myArray = ["karin", "michael", "dan", "yuval", "linoy"];
+console.log(myArray);
+
+//2
+const thirdElement = myArray[2];
+console.log(thirdElement);
+
+//3
+myArray[3] = "joel";
+console.log(myArray);
